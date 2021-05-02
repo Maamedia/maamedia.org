033dcb46a6a329cac551131f2bf0d4243b27f289
Mito <76481138+mitomaamedia@users.noreply.github.com>
2021-05-02 14:05:27 +0300

    404 (not found) page

---
+++ 404.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>404 error | Maamedia</title>
+  <meta charset="utf-8">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Maamedia is a free, open source project that makes free tools and wikis. Made in Finland (Mito) and Indonesia (Puma).">
+    <link rel="stylesheet" href="https://crm.maamedia.org/drafts/christmas/assets/css/global.css">
+<link rel='stylesheet' id='google-font-quicksand-montserrat-poppins-css' href='//fonts.googleapis.com/css?family=Quicksand%3A300%2C400%2C500%2C600%2C700%257CPoppins%3A400%2C400i%2C700%2C700i%257CMontserrat%3A400%2C500%2C600%2C700&#038;subset=latin%2Clatin-ext&#038;display=swap&#038;ver=5.5' media='all'/>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+<style>
+.left, .right {
+  float: left;
+  width: 20%; /* The width is 20%, by default */
+}
+body {
+    font-family: 'Quicksand';
+}
+.main {
+  float: left;
+  width: 60%; /* The width is 60%, by default */
+}
+
+/* Use a media query to add a breakpoint at 800px: */
+@media screen and (max-width: 800px) {
+  .left, .main, .right {
+    width: 100%; /* The width is 100%, when the viewport is 800px or smaller */
+  }
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: white;
+  position: relative;
+}
+
+/* Hide the links inside the navigation menu (except for logo/home) */
+.topnav #maamediaLinks {
+  display: none;
+}
+
+/* Style navigation menu links */
+.topnav a {
+  color: black;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  display: block;
+}
+
+/* Style the hamburger menu */
+.topnav a.icon {
+  background: white;
+  display: block;
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+
+/* Add a grey background color on mouse-over */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Style the active link (or home/logo) */
+.active {
+  background-color: #00000;
+  color: white;
+}
+
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+
+.button.ca {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #4CAF50;
+}
+
+.button.ca:hover {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.button.work {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #f44336;
+}
+
+.button.work:hover {
+  background-color: #f44336;
+  color: white;
+}
+
+
+</style>
+<script>
+function navMaamedia() {
+  var x = document.getElementById("maamediaLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+</script>
+
+<div class="topnav">
+  <a href="#home" class="active"><img src="https://commons.maamedia.org/images/thumb/6/69/Maamedia_Logo_Black.png/120px-Maamedia_Logo_Black.png" style="width:50px; height:auto;">Maamedia</a>
+  <!-- Navigation links (hidden by default) -->
+  <div id="maamediaLinks">
+    <a href="/"><span class="fa fa-fw fa-home"></span> Home</a>
+    <a href="/#ourwork"><span class="fa fa-server"></span> Our work</a>
+    <a href="/contact"><span class="fa fa-inbox"></span> Contact us</a>
+    <a href="/about"><span class="fa fa-info-circle"></span> About us</a>
+    <a href="https://meta.maamedia.org/wiki/Special:CreateAccount"><span class="glyphicon glyphicon-user"></span> Create account</a>
+    <a href="https://meta.maamedia.org/wiki/Special:UserLogin"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+
+  </div>
+  <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+  <a href="javascript:void(0);" class="icon" onclick="navMaamedia()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+
+<div class="header-main">
+<div class="module-404">
+  <h1 style="text-align:center;">404 error</h1>
+  <p>The page you are looking for may have been deleted or moved.</p>
+</div>
+
+
+</div>
+<style>
+.module-404 h1{font-family:Courier,monospace;text-transform:uppercase;font-size:500% !important}.module-404 p,.module-404 .p{font-size:3rem;font-weight:700;max-width:600px;margin:50px auto;line-height:1.13}.link-list{font-size:.8125rem}@media (min-width:768px) and (max-width:1280px){.link-list{font-size:.8125rem}}@media (min-width:1281px){.link-list{font-size:.8125rem}}.link-list.inline-block li{display:inline-block;margin-bottom:0}.link-list>li{list-style-type:none;margin-left:0;clear:both}@media (min-width:768px){.link-list>li{margin-bottom:60px}}.link-list.hover-highlight a{background-image:linear-gradient(to top,#05e9d1,#05e9d1);background-size:100% .375rem;background-position:bottom;background-repeat:no-repeat;transition:.3s all;font-weight:700;padding-bottom:5px;line-height:1.8}.link-list.hover-highlight a:hover{background-size:100% 2rem;color:#000}@media (min-width:768px){.link-list.hover-highlight a{background-size:100% .375rem}.link-list.hover-highlight a:hover{background-size:100% 2rem}}.link-list.hover-highlight_dk a{background-image:linear-gradient(to top,#3625d1,#3625d1);background-size:100% .375rem;background-position:bottom;background-repeat:no-repeat;transition:.3s all;font-weight:700;padding-bottom:5px;line-height:1.8}.link-list.hover-highlight_dk a:hover{background-size:100% 2rem;color:#fff}@media (min-width:768px){.link-list.hover-highlight_dk a{background-size:100% .375rem}.link-list.hover-highlight_dk a:hover{background-size:100% 2rem}}.link-list a{color:#3a25ff;display:inline-flex}.link-list a img{height:16px;margin-right:5px}.profile-contacts{font-size:13px;color:#3a25ff;margin-bottom:20px}.profile-contacts img{height:16px;margin-right:6px;margin-top:1px}.profile-contacts a:hover{text-decoration:underline}.list-inline li{display:inline;text-decoration:none}.toc{top:75px;bottom:0;padding-right:20px}.admin-bar
+</style>
+</body>
+</html>
