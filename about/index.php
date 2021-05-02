6e120dae0087a40356ebe2534ffd9d1599eacb1a
Mito <76481138+mitomaamedia@users.noreply.github.com>
2021-05-02 13:10:55 +0300

    about page

---
+++ about/index.php
@@ -0,0 +1,545 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Contact | Maamedia</title>
+  <meta charset="utf-8">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Maamedia is a free, open source project that makes free tools and wikis. Made in Finland (Mito) and Indonesia (Puma).">
+<link rel='stylesheet' id='google-font-quicksand-montserrat-poppins-css' href='//fonts.googleapis.com/css?family=Quicksand%3A300%2C400%2C500%2C600%2C700%257CPoppins%3A400%2C400i%2C700%2C700i%257CMontserrat%3A400%2C500%2C600%2C700&#038;subset=latin%2Clatin-ext&#038;display=swap&#038;ver=5.5' media='all'/>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+<style>
+
+body {
+    font-family: 'Quicksand';
+}
+
+
+/* Use a media query to add a breakpoint at 800px: */
+
+* {
+  box-sizing: border-box;
+}
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+  position: relative;
+}
+
+/* Hide the links inside the navigation menu (except for logo/home) */
+.topnav #maamediaLinks {
+  display: none;
+}
+
+/* Style navigation menu links */
+.topnav a {
+  color: white;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  display: block;
+}
+
+/* Style the hamburger menu */
+.topnav a.icon {
+  background: black;
+  display: block;
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+
+/* Add a grey background color on mouse-over */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Style the active link (or home/logo) */
+.active {
+  background-color: #00000;
+  color: white;
+}
+
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+
+.button.ca {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #4CAF50;
+}
+
+.button.ca:hover {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.button.work {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #f44336;
+}
+
+.button.work:hover {
+  background-color: #f44336;
+  color: white;
+}
+
+
+</style>
+<script>
+function navMaamedia() {
+  var x = document.getElementById("maamediaLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+</script>
+
+<div class="topnav">
+  <a href="#home" class="active"><img src="https://commons.maamedia.org/images/1/1a/Maamedia_Logo.png" style="width:50px; height:auto;">About Maamedia</a>
+  <!-- Navigation links (hidden by default) -->
+  <div id="maamediaLinks">
+    <a href="/"><span style="font-size:13px;" class="fa fa-fw fa-home"></span> Home</a>
+    <a href="/#ourwork"><span style="font-size:13px;" class="fa fa-server"></span> Our work</a>
+    <a href="/contact"><span style="font-size:13px;" class="fa fa-inbox"></span> Contact us</a>
+    <a href="/about"><span style="font-size:13px;" class="fa fa-info-circle"></span> About us</a>
+    <a href="https://meta.maamedia.org/wiki/Special:CreateAccount"><span style="font-size:13px;" class="glyphicon glyphicon-user"></span> Create account</a>
+    <a href="https://meta.maamedia.org/wiki/Special:UserLogin"><span style="font-size:13px;" class="glyphicon glyphicon-log-in"></span> Login</a>
+
+  </div>
+  <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+  <a href="javascript:void(0);" class="icon" onclick="navMaamedia()">
+    <i style="font-size:17px;" class="fa fa-bars"></i>
+  </a>
+</div>
+<header class="text-white text-center">
+
+<h1 class="masthead-heading text-uppercase mb-0">
+					About			</h1>
+					<style>
+					.blue {
+					    color: blue;
+					}
+					.red {
+					    color: red;
+					}
+					.green {
+					    color: green;
+					}
+					.yellow {
+					    color: yellow;
+					}
+					</style>
+<h1>Maamedia mission is <span class="blue">help people</span> to <span class="red">learn new things</span> easily and to <span class="green">succeed</span> in the life and make them <span class="yellow">happy</span>.</h1>
+
+
+        <p>Masymanwiki is guidebook and made for you.</p>
+        <h4>Maamedia tools help people in everyday matters and businesses in many different things.</h4>
+        <h4>Plus, Mawonder, a social media network, is a service just for you.</h4>
+        <a href="/products"><p>Learn more Maamedia products</p></a>
+        
+<div class="ct-pageWrapper" id="ct-js-wrapper">
+   <div class="container">
+
+         </div>
+      </div>
+<section class="story-section company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding" id="section">
+   <div class="container text-center">
+      <h2>
+         A short story about us.
+      </h2>
+      <h3>
+         Why we are.
+      </h3>
+      <div class="col-md-8 col-md-offset-2">
+         <div class="red-border">
+            &nbsp;
+         </div>
+         <p class="ct-u-size22 ct-u-fontWeight300 marginTop40">
+            Maamedia was founded so that people could learn to do things. To this end, Maamedia wiki Masymanwiki was also created, alongside other sister wikis. Later, Maamedia began to develop free tools online as well as a later social network such as forum and Mawonder. Maamedia will start accepting donations in June 2021 so that it can continue to be open and free to everyone. Maamedia pays the most for servers that would not exist without Maamedia.
+         </p>
+         <!-- <a class="ct-u-marginTop60 btn btn-solodev-red btn-fullWidth-sm ct-u-size19" href="#">Learn More</a> -->
+      </div>
+   </div>
+</section>
+<section class="culture-section company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding">
+   <div class="container">
+      <div class="col-md-8 col-md-offset-2">
+         <h2>
+            Short Features. 
+         </h2>
+         <h3>
+            Why choose Maamedia?
+         </h3>
+         <p class="ct-u-size22 ct-u-fontWeight300 ct-u-marginBottom60">
+            We want our consumers and users to be satisfied with our services, ie our products.<br>
+            Because of this, we have made some handy add-ons to our products. 
+         </p>
+      </div>
+      <div class="row ct-u-paddingBoth20">
+         <div class="col-xs-6 col-md-4">
+            <div class="company-icons-white">
+               <i class="fa fa-medkit" aria-hidden="true"></i>
+               <p>
+                  EFFECTIVE
+               </p>
+               <p class="company-icons-subtext hidden-xs">
+                  We take care of the servers, so efficiency is seen when using the services.
+               </p>
+            </div>
+         </div>
+         <div class="col-xs-6 col-md-4">
+            <div class="company-icons-white">
+               <i class="fa fa-money" aria-hidden="true"></i>
+               <p>
+                  FREE FOR YOU
+               </p>
+               <p class="company-icons-subtext hidden-xs">
+                  Reading or using the services will not cost you anything. Instead, donations that you too can make will cost you little.
+               </p>
+            </div>
+         </div>
+         <div class="col-xs-6 col-md-4">
+            <div class="company-icons-white">
+               <i class="fa fa-clock-o" aria-hidden="true"></i>
+               <p>
+                  SHORT RESPONSE
+               </p>
+               <p class="company-icons-subtext hidden-xs">
+                  We have streamlined response time with code cleanup and other maintenance work. So the services are fast. 
+               </p>
+            </div>
+         </div>
+      </div>
+      <div class="row ct-u-paddingBoth20">
+         <div class="col-xs-6 col-md-4">
+            <div class="company-icons-white">
+               <i class="fa fa-info" aria-hidden="true"></i>
+               <p>
+                  24/7 SUPPORT
+               </p>
+               <p class="company-icons-subtext hidden-xs">
+                  If you need help, you will always be served in all matters.
+               </p>
+            </div>
+         </div>
+         <div class="col-xs-6 col-md-4">
+            <div class="company-icons-white">
+               <i class="fa fa-server" aria-hidden="true"></i>
+               <p>
+                  MUCH SERVICES AND PRODUCTS
+               </p>
+               <p class="company-icons-subtext hidden-xs">
+                  Services and products can be found for all people and also for companies.
+               </p>
+            </div>
+         </div>
+         <div class="col-xs-6 col-md-4">
+            <div class="company-icons-white">
+               <i class="fa fa-thumbs-up" aria-hidden="true"></i>
+               <p>
+                  MAWONDER
+               </p>
+               <p class="company-icons-subtext hidden-xs">
+                  Mawonder is a free, feature-rich social media service. It is easy and fast, and also available for Android phones.
+               </p>
+            </div>
+         </div>
+      </div>
+      <a class="ct-u-marginTop60 btn btn-solodev-red-reversed btn-fullWidth-sm ct-u-size19" href="/products">See all Maamedia products</a>
+   </div>
+</section>
+                  </div>
+               </div>
+            </section>
+         </div>
+      </div>
+   </div>
+        
+        
+        
+        <style>
+        .separator-type, .title-type, .intro-type, .content-type .intro-type p {
+    position: relative;
+}
+.intro-type .container {
+    padding: 107px 0px 102px 0px !important;
+}
+#product-header-section, .intro-type .container {
+    padding: 68px 15px 72px !important;
+}
+@media (min-width: 1630px)
+.container {
+    width: 1630px;
+}
+.parallax {
+    background-attachment: inherit !important;
+}
+.parallax {
+    background-attachment: inherit !important;
+    background-repeat: repeat;
+    background-size: cover;
+    position: absolute;
+    top: 0px;
+    bottom: 0px;
+    width: 100%;
+    z-index: -10;
+}
+.company-heading h1 {
+    margin-bottom: 40px;
+    line-height: 80px;
+    color: #fff;
+    font-weight: 700 !important;
+    text-align: center;
+}
+.seo-header, .product-header, .intro-type .container h1.white, .company-heading h1 {
+    font-family: 'Raleway', sans-serif;
+    font-weight: 100 !important;
+    text-transform: capitalize;
+    font-size: 65px;
+    margin-bottom: 20px;
+}
+.company-sections h2, .careers-sections h2, .perks-section h2 {
+    font-weight: 800;
+    font-size: 50px;
+}
+.company-sections h2, .company-sections h3, .careers-sections h2, .perks-section h2 {
+    font-family: 'proxima-nova', sans-serif;
+}
+.company-sections h3 {
+    font-size: 25px;
+    font-weight: 700;
+    margin: 14px 0;
+    font-family: 'proxima-nova', sans-serif;
+    text-transform: capitalize;
+}
+.red-border {
+    width: 50%;
+    border-bottom: 1px solid #d2282e;
+    margin: 0 auto;
+    height: 16px;
+}
+.company-sections p {
+    margin-top: 20px;
+}
+.ct-u-size22 {
+    font-size: 22px;
+}
+.ct-u-fontWeight300 {
+    font-weight: 300;
+}
+.marginTop40 {
+    margin-top: 40px !important;
+}
+.ct-u-paddingBoth100 {
+    padding: 100px 15px;
+}
+.culture-section {
+    background: linear-gradient(rgba(0, 0, 0, 0.45),rgba(0, 0, 0, 0.45)), url(https://www.solodev.com/assets/culture.jpg) top no-repeat;
+    /* background-color: #095c87; */
+    /* color: #fff; */
+}
+.company-sections, .careers-sections, .left-headquarter-section-img, .right-headquarter-section-img {
+    text-align: center;
+}
+.ct-u-paddingBoth100 {
+    padding: 100px 15px;
+}
+.company-sections h2, .careers-sections h2, .perks-section h2 {
+    font-weight: 800;
+    font-size: 50px;
+}
+.company-sections h2, .company-sections h3, .careers-sections h2, .perks-section h2 {
+    font-family: 'proxima-nova', sans-serif;
+}
+.slick-slider {
+    margin-bottom: 0;
+}
+.slick-slider {
+    position: relative;
+    display: block;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-touch-callout: none;
+    -khtml-user-select: none;
+    -ms-touch-action: pan-y;
+    touch-action: pan-y;
+    -webkit-tap-highlight-color: transparent;
+}
+.slick-slider .slick-track, .slick-slider .slick-list {
+    -webkit-transform: translate3d(0, 0, 0);
+    -moz-transform: translate3d(0, 0, 0);
+    -ms-transform: translate3d(0, 0, 0);
+    -o-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+}
+.slick-list {
+    position: relative;
+    display: block;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+}
+.slick-track {
+    position: relative;
+    top: 0;
+    left: 0;
+    display: block;
+}
+.slider .item {
+    overflow: hidden;
+}
+.slick-slide {
+    display: none;
+    float: left;
+    height: 100%;
+    min-height: 1px;
+}
+.logos {
+    margin-right: -5px;
+    margin-left: -5px;
+}
+ul.logos li {
+    cursor: pointer;
+}
+.logos > li {
+    float: left;
+    width: 16.66666%;
+    padding: 5px;
+}
+.logos .logos-inner {
+    position: relative;
+}
+.slick-slide img {
+    display: inline-block;
+}
+.logo-image {
+    position: relative;
+    width: 100%;
+}
+ul.logos li {
+    cursor: pointer;
+}
+.logos > li {
+    float: left;
+    width: 16.66666%;
+    padding: 5px;
+}
+.logos .logos-inner {
+    position: relative;
+}
+.slick-slide img {
+    display: inline-block;
+}
+
+.fa {
+  font-size: 42px;
+}
+.btn-solodev-red-reversed {
+    background-color: #fff;
+    color: #d2282e;
+    -webkit-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+    padding: 12px 35px;
+}
+.btn, .btn-blk {
+    font-size: 18px !Important;
+}
+a:hover, a:focus {
+    text-decoration: none;
+}
+.ct-u-size19 {
+ margin-top: 40px; 
+}
+a:hover {
+    color: #000;
+}
+section.clients-home .clients-logos .client-logos-repeater {
+    height: 260px;
+    border-right: 1px solid #ccc;
+    border-bottom: 1px solid #ccc;
+    cursor: pointer;
+}
+
+section.clients-home .clients-logos .client-logos-repeater img {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    left: 0;
+    right: 0;
+}
+.logo-title {
+    position: absolute;
+    display: none;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 16px;
+    text-transform: uppercase;
+    color: #FFF;
+    background-color: #0079c2;
+}
+.ct-u-paddingBottom100 {
+  margin-top: 40px;
+}
+.culture-section {
+  background-size: cover;
+  color: #fff;
+}
+        </style>
+        
+<!--Footer-->
+<footer style="height:auto; color:white; background-color:black">
+  <h1 style="color:white; background-color:black; padding:40px; margin:0; text-align:center;">Made with 🤍 Maamedia</h1>
+<div style="border-radius: 3px; max-width: 800px; padding: 15px; margin:auto;margin-top: 15px;color: #7b7b7b;" align="center">
+         <i style="font-size:13px;" class="fa fa-language"></i> <a href="https://meta.maamedia.org/wiki/Special:Translate?group=Maamedia.org">Help us with the translations</a> 
+    <div style="font-size:140%">Legal information</div>
+         <a href="/privacy-policy">Privacy Policy</a><br>
+         <a href="/tos">Terms of use</a><hr style="margin: 8px;">
+    <div style="font-size:140%">Contact to Maamedia</div>
+    <i style="font-size:13px;" class="fa fa-envelope"></i> <a href="mailto:info@maamedia.org">info@maamedia.org</a><hr style="margin: 8px;">
+    <div style="font-size:140%">Get help</div>
+        <a href="/faq">FAQ</a><br>
+        <a href="https://crm.maamedia.org/support">Support</a><hr style="margin: 8px;">
+    <div style="font-size:140%">Source code</div>
+    <i style="font-size:13px;" class="fa fa-github"></i> <a href="https://github.com/maamedia">Github</a><br>
+        <a href="/coding">Code with us</a>
+    
+        </div>
+</footer>
+</body>
+</html>
