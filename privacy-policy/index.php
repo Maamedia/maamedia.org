35a060fe0a8026aa2e6851a95de343433bfc692a
Mito <76481138+mitomaamedia@users.noreply.github.com>
2021-06-19 22:17:25 +0300

    privacy policy page = https://maamedia.org/privacy-policy/

---
+++ privacy-policy/index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Privacy Policy | Maamedia</title>
+  <html lang="en">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Maamedia is a free, open source project that makes free tools and wikis. Made in Finland (Mito) and Indonesia (Puma).">
+    <link rel="stylesheet" href="https://crm.maamedia.org/drafts/christmas/assets/css/global.css">
+<link rel='stylesheet' id='google-font-quicksand-montserrat-poppins-css' href='//fonts.googleapis.com/css?family=Quicksand%3A300%2C400%2C500%2C600%2C700%257CPoppins%3A400%2C400i%2C700%2C700i%257CMontserrat%3A400%2C500%2C600%2C700&#038;subset=latin%2Clatin-ext&#038;display=swap&#038;ver=5.5' media='all'/>
+<link rel="copyright" href="//creativecommons.org/licenses/by-sa/3.0/">
+<link rel="license" href="//creativecommons.org/licenses/by-sa/3.0/">
+<link rel="shortcut icon" href="https://commons.maamedia.org/images/1/1a/Maamedia_Logo.png">
+<link rel="apple-touch-icon" href="https://commons.maamedia.org/images/1/1a/Maamedia_Logo.png">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+<style>
+.left, .right {
+  float: left;
+  width: 20%; /* The width is 20%, by default */
+}
+body {
+    font-family: 'Quicksand';
+}
+.main {
+  float: left;
+  width: 60%; /* The width is 60%, by default */
+}
+
+/* Use a media query to add a breakpoint at 800px: */
+@media screen and (max-width: 800px) {
+  .left, .main, .right {
+    width: 100%; /* The width is 100%, when the viewport is 800px or smaller */
+  }
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+  position: relative;
+}
+
+/* Hide the links inside the navigation menu (except for logo/home) */
+.topnav #maamediaLinks {
+  display: none;
+}
+
+/* Style navigation menu links */
+.topnav a {
+  color: white;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  display: block;
+}
+
+/* Style the hamburger menu */
+.topnav a.icon {
+  background: black;
+  display: block;
+  position: absolute;
+  right: 0;
+  top: 8px;
+}
+
+/* Add a grey background color on mouse-over */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Style the active link (or home/logo) */
+.active {
+  background-color: #00000;
+  color: white;
+}
+
+.mahr { margin-before: 0.0em; margin-after: 0.0em; margin-start: auto; margin-end: auto; overflow: hidden; border-style: inset; border-width: 4px; color: black;}
+
+.content {
+    font-size: 140%;
+    text-align: left;
+    padding-left: 50px;
+    margin-bottom: 2rem;
+    color: black;
+    position: relative;
+
+}
+
+</style>
+<script>
+function navMaamedia() {
+  var x = document.getElementById("maamediaLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+</script>
+
+<div class="topnav">
+  <a href="#home" class="active"><img src="https://commons.maamedia.org/images/1/1a/Maamedia_Logo.png" style="width:50px; height:auto;">Maamedia Privacy Policy</a>
+  <!-- Navigation links (hidden by default) -->
+  <div id="maamediaLinks">
+    <a href="/"><span class="fa fa-fw fa-home"></span> Home</a>
+    <a href="/#ourwork"><span class="fa fa-server"></span> Our work</a>
+    <a href="/contact"><span class="fa fa-inbox"></span> Contact us</a>
+    <a href="/about"><span class="fa fa-info-circle"></span> About us</a>
+    <a href="https://meta.maamedia.org/wiki/Special:CreateAccount"><span class="glyphicon glyphicon-user"></span> Create account</a>
+    <a href="https://meta.maamedia.org/wiki/Special:UserLogin"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+
+  </div>
+  <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+  <a href="javascript:void(0);" class="icon" onclick="navMaamedia()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+<header class="text-white text-center">
+
+<h1 class="masthead-heading text-uppercase mb-0">
+					Maamedia Non-wiki Privacy Policy			</h1>
+<div class="mahr"></div>
+<div class="content">
+Hello!<br><br>
+This is the non-wiki privacy policy of the Maamedia. This policy applies to non-Maamedia wikis, but some Maamedia products (such as social networks) may have different sections. You can see the privacy policy of Maamedia wikis <a href="https://meta.maamedia.org/wiki/Privacy_Policy">here</a>.
+<br><br>
+<h3>What information Maamedia collects when it visits its website</h3>
+Whenever you visit any website of Maamedia (such as maamedia.org) or any other sites, including wikis, some information about you will be stored. This information is as follows:
+    <li>IP address</li>
+    <li>Browser</li>
+    <li>Timezone and country</li>
+    <li>Visiting time</li>
+    <li>Pages where you visit</li>
+This information is stored in a secret file, which can only be accessed via the command line with a password and cannot be found by a browser or robot. For example, if you are registered on a social network, more information about you will be stored, which you can find more on the <a href="https://maamedia.org/legal/">legal</a> page or on the social network policy pages.
+<br><br>
+<h3>What the data is used for</h3>
+Non-personal information collected about you (such as pages you visit, time zone, and browser) is used to improve your user experience and to display the right content. Your visits are never told to outsiders, and no one has a right to know about it.
+<br>
+We use your information to personalize and check the services, which are the most popular pages and what should be on them.
+<br>
+<h3>How long the data is kept</h3>
+Generally, your information on pages visited by Maamedia will be retained for three weeks until the file is cleaned and begins to collect new data. Data can also be retained for a much shorter period of time if the sites have several thousand visitors per day. <br>The file generally allows information for about 6,000 visitors, and the file will be automatically emptied whenever it is exceeded or no later than three (3) weeks. The visitor does not have the right to request the deletion of the data as it will happen automatically in a short time. If Maamedia were to start deleting the information of those who wanted it, it would become complicated and it would complicate the flow of information.
+<br>
+<h3>Who can see your information</h3>
+Maamedia is still an exceptionally new project (founded at the end of January 2021). This means that Maamedia does not yet have many people, such as system administrators. <br>Currently, your information can only be viewed by the founders of Maamedia, who, of course, do not have the right to share your information with outsiders in non-emergency cases (listed below).
+In other cases (if Maamedia had them), system and server administrators would be able to see your information.<br>
+Your information can actually be viewed at any time on behalf of the founders, but this is not done. Usually, when you view your data, you are mainly looking at statistics and the most read pages. In addition, they design and make diagrams of what kind of content should be displayed more and what less, for example in different countries.
+<br>
+<h3>Where your information is stored</h3>
+As mentioned above, your information is located in a separate file that can only be accessed with a password and command line languages. It is therefore impossible to open it to outsiders. The file does not contain any information that would be harmful to you or your privacy.
+<br>
+<h3>Emergencies where your information may be disclosed to other parties</h3>
+There is a specific policy on when and to whom information collected about you may be disclosed. Listed here are so-called emergencies where your information may be revealed. 
+<li>The IP address has been blocked from Maamedia wikis due to a violation of national law.</li>
+<li>You have tried to leak illegal or vague material to our sites.</li>
+<li>You make a visit to an area where Internet use is 1. restricted or 2. completely prohibited.</li>
+<li>Your IP address matches a case that has already been notified to the parties.</li>
+This information may be disclosed to following parties:
+    <li>The local police organization in your country</li>
+    <li>FBI</li>
+    <li>To the community or organization concerned</li>
+    <li>For a lawyer</li>
+    <li>If you have an open proxy (VPN), the IP address you use will be reported for abuse</li><br>
+The information will be disclosed by e-mail and, if necessary, screenshots or complete passwords will be sent to access Maamedia servers.<br>
+<h3>Changes to this Privacy Policy</h3>
+Maamedia, its founders and the Privacy Policy Officer (currently the founders) have the right to modify this policy without notice.<br><br>
+
+Last modified: 6. May 2021
+</div>
+<!-- ADD CONTENT -->
+
+
+<!--Footer-->
+<footer style="height:auto; color:white; background-color:black">
+  <h1 style="color:white; background-color:black; padding:40px; margin:0; text-align:center;">Made with 🤍 Maamedia</h1>
+<div style="border-radius: 3px; max-width: 800px; padding: 15px; margin:auto;margin-top: 15px;color: #7b7b7b;" align="center">
+         <i class="fa fa-language"></i> <a href="https://meta.maamedia.org/wiki/Special:Translate?group=Maamedia.org">Help us with the translations</a> 
+    <div style="font-size:140%">Legal information</div>
+         <a href="/legal">Legal Page</a><br>
+         <a href="/privacy-policy">Privacy Policy</a><br>
+         <a href="/tos">Terms of use</a><hr style="margin: 8px;">
+    <div style="font-size:140%">Contact to Maamedia</div>
+    <i class="fa fa-envelope"></i> <a href="mailto:info@maamedia.org">info@maamedia.org</a><hr style="margin: 8px;">
+    <div style="font-size:140%">Get help</div>
+        <a href="/faq">FAQ</a><br>
+        <a href="https://crm.maamedia.org/support">Support</a><hr style="margin: 8px;">
+    <div style="font-size:140%">Source code</div>
+    <i class="fa fa-github"></i> <a href="https://github.com/maamedia">Github</a><br>
+        <a href="/coding">Code with us</a>
+    
+        </div>
+</footer>
+</body>
+</html>
